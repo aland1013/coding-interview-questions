b452d944ea3f1edc190f34764a6d791573d710b5
aland1013 <adam.land@gmail.com>
2021-03-23 20:55:14 -0400

    solved 4.8 first common ancestor

--- chapter-4/4.8-first-common-ancestor.js
+++ chapter-4/4.8-first-common-ancestor.js
@@ -6,25 +6,74 @@ in a data structure.
 
 Example:
 
- 10
- /
-1   2   4  11
- \ /   / \ /
-  3   5   8
-   \ / \   \
-    6   7   9
+     1
+   /   \
+  2     3
+ / \   / \
+4   5 6   7
+
+FCA (4, 5) = 2
+FCA (4, 6) = 1
+FCA (3, 4) = 1
+FCA (2, 4) = 2
 
 PLAN: 
-1. Translate into graph terminology:
-- vertices: each individual in the dataset
-- edges: connect children to parents
-- weights: n/a
-- path: youngest to oldest, generation by generation
-2. Build graph:
-- adjacency list with each individual as a key and the value is a set of the parents
-3. Traverse graph:
-- farthest distance -> DFT
-- create a hash table that stores the paths from the starting node
-4. Compare paths:
-- given two vertices, find and return the last (rightmost) common value in their paths, else return 'none'
+1. Find path from root to n1 and store it in an array
+2. Find path from root to n2 and store it in an array
+3. Traverse both paths until the values in the arrays are the same.
+   Return the common element immediately before the mismatch.
 */
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+const findPath = (root, path, v) => {
+  if (!root) return false;
+
+  path.push(root.value);
+
+  if (root.value == v) return true;
+
+  if (
+    (root.left && findPath(root.left, path, v)) ||
+    (root.right && findPath(root.right, path, v))
+  )
+    return true;
+
+  path.pop();
+  return false;
+};
+
+const findFCA = (root, n1, n2) => {
+  let path1 = [];
+  let path2 = [];
+
+  if (!findPath(root, path1, n1) || !findPath(root, path2, n2)) return -1;
+
+  let i = 0;
+  while (i < path1.length && i < path2.length) {
+    if (path1[i] !== path2[i]) {
+      break;
+    }
+    i++;
+  }
+
+  return path1[i - 1];
+};
+
+const root = new Node(1);
+root.left = new Node(2);
+root.right = new Node(3);
+root.left.left = new Node(4);
+root.left.right = new Node(5);
+root.right.left = new Node(6);
+root.right.right = new Node(7);
+
+console.log('FCA(4, 5) = ', findFCA(root, 4, 5));
+console.log('FCA(4, 6) = ', findFCA(root, 4, 6));
+console.log('FCA(3, 4) = ', findFCA(root, 3, 4));
+console.log('FCA(2, 4) = ', findFCA(root, 2, 4));
